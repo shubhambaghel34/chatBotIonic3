5e691eb1d47d5ea16747dcd7b9e8cbf92d65a046
Shubham_Baghel <shubhambaghel34@gmail.com>
2019-08-01 18:50:43 +0530

    Updated code for UI

---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export class Constants{
+    //Add here Global constants,variables,URLS 
+    public static readonly API_URL='http://172.30.24.54:8081/xiva';
+
+}
--- src/providers/chatservice/chatservice.ts
+++ src/providers/chatservice/chatservice.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Events } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import{Constants} from '../../constants/constants';
 /*
   Generated class for the ChatserviceProvider provider.
 
@@ -10,83 +11,50 @@ import { map } from 'rxjs/operators';
   and Angular DI.
 */
 export class ChatMessage {
-  // messageId: string;
-  // userId: string;
-  // // userName: string;
-  // // userAvatar: string;
-  // toUserId: string;
-  // time: number | string;
-  message: string;
-  // status: string;
-}
+   message: string;
+   userName: string;
+  
+  }
 
 export class UserInfo {
-  id: string;
+ id:string;
+  name?: string;
 }
 
-export const userAvatar = '../../assets/imgs/chatbot.png';
-export const toUserAvatar = '../../assets/imgs/logo.png';
+
 
 
 @Injectable()
 export class ChatserviceProvider {
   objdata: any = [];
+  getUserInfo():Promise<UserInfo> {
+    const userInfo: UserInfo = {
+      id: '',
+      name: 'User',
+    
+    };
+    return new Promise(resolve => resolve(userInfo));
+  }
 
+ 
   constructor(public http: HttpClient, private events: Events) {
     console.log('Hello ChatserviceProvider ');
   }
-  // mockNewMsg(msg) {
-  //   const mockMsg: ChatMessage = {
-  //     // messageId: Date.now().toString(),
-  //     // userId: '210000198410281948',
-  //     // toUserId: '140000198202211138',
-  //     message: msg.message,
-  //     // status: 'success'
-  //   };
-
-  //   setTimeout(() => {
-  //     this.events.publish('chat:received', mockMsg, Date.now())
-  //   }, Math.random() * 1800)
-  // }
-  // getMessageList() {
-  //   const msgListUrl = 'http://172.30.24.54:8080/xiva';
-  //   var promise = new Promise((resolve, reject) => {
-  //     this.http.post(msgListUrl,{query:'hi'}).subscribe((response) => {
-  //       resolve(response);
-  //   });
-  //   });
-  //   return promise;  
-  // }
-
-
-  //   getUserInfo():Promise<UserInfo> {
-  //     const userInfo: UserInfo = {
-  //       id: '140000198202211138',
-
-  //     };
-  //     return new Promise(resolve => resolve(userInfo));
-  //   }
-
-
-  // }
+  
   sendMsg(msg: ChatMessage) {
-    const msgListUrl = 'http://172.30.24.54:8080/xiva';
+  
+    const msgListUrl = Constants.API_URL;
     var promise = new Promise((resolve, reject) => {
-      // const mockMsg: ChatMessage = {
-      //       // messageId: Date.now().toString(),
-      //       // userId: '210000198410281948',
-      //       // toUserId: '140000198202211138',
-      //       message: msg.message + '123',
-      //       // status: 'success'
-      //  };
-      //  resolve(mockMsg);
-       
-      this.http.post(msgListUrl,{ query: msg.message }).subscribe((response) => {
+      this.http.post(msgListUrl, { query: msg.message }).subscribe((response) => {
         resolve(response);
       });
     });
     return promise;
   }
+
+
+ 
+
 
 }
 
